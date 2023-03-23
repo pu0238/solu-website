c1969ce11cd04b89511c714fa1d2155ae966228d
pu0238 <michal.lustyk@blocklabs.eu>
2023-03-23 21:00:29 +0100

    Added site heading

---
+++ src/components/HeaderTitle.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 class="text-6xl font-cal"
+        :class="{
+          'text-black': textColor === 'white',
+          'text-white': textColor === 'black',
+        }"
+  >
+    <span
+      class="relative"
+      :class="{
+        'text-white text-stroke-black-3': textColor === 'white',
+        'text-black text-stroke-white-3': textColor === 'black',
+      }"
+      >Comprehensive software</span
+    >
+    for the 21st century!
+  </h1>
+</template>
+
+<script lang="ts">
+import { reactive } from "vue";
+
+export default {
+  name: "NavItem",
+  props: {
+    textColor: {
+      type: String,
+      default: "black",
+      required: true,
+      validator: (color: string) => ["white", "black"].includes(color),
+    },
+  },
+  setup(props) {
+    props = reactive(props);
+    return {};
+  },
+};
+</script>
